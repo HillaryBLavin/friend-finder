2ea81b9d043d550d0f302c11e3a0ca860da5c832
Hillary Lavin <hilavin@gmail.com>
2018-05-11 09:29:54 -0400

    Rename friends > adventurers, add 3 characters to array

---
+++ app/data/adventurers.js
@@ -0,0 +1,52 @@
+var adventurers = [
+    {
+        name: "Frodo Baggins",
+        photo: "https://upload.wikimedia.org/wikipedia/en/4/4e/Elijah_Wood_as_Frodo_Baggins.png",
+        scores: [
+            "5",
+            "1",
+            "3",
+            "2",
+            "4",
+            "3",
+            "5",
+            "1",
+            "3",
+            "5"
+        ]
+    },
+    {
+        name: "Usidore, Wizard of the 12th Realm of Ephysiyies, Master of Light and Shadow, Manipulator of Magical Delights, Devourer of Chaos, Champion of the Great Halls of Terr'akkas. The elves know me as Fi’ang Yalok. The dwarfs know me as Zoenen Hoogstandjes. And I am also known in the Northeast as Gaismunēnas Meistar.",
+        photo: "http://magictavern.wdfiles.com/local--files/usidore/usidore-the-blue.jpg",
+        scores: [
+            "2",
+            "4",
+            "4",
+            "1",
+            "1",
+            "5",
+            "1",
+            "3",
+            "5",
+            "2"
+        ]
+    },
+    {
+        name: "Magnus Burnsides",
+        photo: "https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/6e735662111263.5a8536afa1c90.jpg",
+        scores: [
+            "1",
+            "3",
+            "5",
+            "3",
+            "5",
+            "2",
+            "1",
+            "4",
+            "1",
+            "5"
+        ]
+    }
+];
+
+module.exports = adventurers;
